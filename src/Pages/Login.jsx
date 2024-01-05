ec991518666242d1c2018d50ec424f35023e99ae
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2024-01-05 15:44:50 +0530

    add loader

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -49,11 +49,14 @@ const Login = () => {
     console.log("Login Data:", loginData);
 
     setTimeout(() => {
-        if(!isValidEmail(loginData.email) || !isValidPassword(loginData.password)){
-            setLoading(false);
-            setErrorEmail(true);
-            setErrorPassword(true);
-        }
+      if (
+        !isValidEmail(loginData.email) ||
+        !isValidPassword(loginData.password)
+      ) {
+        setLoading(false);
+        setErrorEmail(true);
+        setErrorPassword(true);
+      }
       if (!isValidEmail(loginData.email)) {
         setLoading(false);
         setErrorEmail(true);
@@ -64,40 +67,9 @@ const Login = () => {
         setLoading(false);
         alert("Login Successful");
       }
-
     }, 2000);
   };
 
-  if (loading) {
-    return (
-      <>
-        <div class="absolute bg-opacity-60 z-10 h-full w-full flex items-center justify-center">
-          <div class="flex items-center">
-            <svg
-              class="animate-spin h-8 w-8 text-gray-800"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </div>
-        </div>
-      </>
-    );
-  } else {
     return (
       <>
         <div className="bg-white flex justify-center items-center h-screen">
@@ -172,18 +144,34 @@ const Login = () => {
               </div>
 
               <div className="relative w-full mt-6">
-                <button
-                  type="submit"
-                  className={`${
-                    loginData.email === "" || loginData.password === ""
-                      ? "bg-blue-100 cursor-not-allowed"
-                      : "bg-blue-600"
-                  }  text-white font-semibold px-6 w-28 h-8 border rounded-sm`}
-                  onClick={handleLogin}
-                  disabled={loginData.email === "" || loginData.password === ""}
-                >
-                  Sign in
-                </button>
+                {loading ? (
+                  <>
+                    <img
+                      class="h-12 w-12"
+                      src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif"
+                      alt=""
+                    />
+                  </>
+                ) : (
+                  <>
+                    <button
+                      type="submit"
+                      className={`${
+                        loginData.email === "" || loginData.password === ""
+                          ? "bg-blue-100 cursor-not-allowed"
+                          : "bg-blue-600"
+                      }  text-white font-semibold px-6 w-28 h-8 border rounded-sm`}
+                      onClick={handleLogin}
+                      disabled={
+                        loginData.email === "" || loginData.password === ""
+                      }
+                    >
+                      <div className="flex items-center justify-center">
+                        Sign in
+                      </div>
+                    </button>
+                  </>
+                )}
 
                 <div className="text-blue-500 absolute inset-y-0 right-0 flex items-center px-4">
                   <span className="hover:underline text-sm">
@@ -219,7 +207,7 @@ const Login = () => {
         </div>
       </>
     );
-  }
+  
 };
 
 export default Login;
